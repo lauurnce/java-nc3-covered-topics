49e3ac377059e44ea9288d5a9041f005e7bbe276
Your Full Name <paneslawrence8@gmail.com>
2025-09-06 13:05:16 +0800

    inheritance

---
+++ day25Vehicle_ParentClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysitvi.inheritance.day25;
+
+/**
+ *
+ * @author Admin
+ */
+//Parent Class / Super Class
+public class Vehicle {
+    int countOfWheels;
+    String brand; 
+    String color; 
+    
+    public void accelerate(){
+        System.out.println("Lawrence");
+    }
+    
+    
+}
