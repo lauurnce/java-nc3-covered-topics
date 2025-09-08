af63f9a40f1db578514e27cb65976d76996bc83f
Lawrence Panes <paneslawrence8@gmail.com>
2025-09-08 12:53:45 +0800

    parent class - act 11

--- day25SolarSystem.java
+++ day25SolarSystem.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.joysistvi.oopactivity.day25;
 
-/**
- *
- * @author Admin
- */
 public class SolarSystem {
+    String name; 
+    String type;
     
+    public void information(){
+        System.out.println("Planet: " + name);
+    }
 }
