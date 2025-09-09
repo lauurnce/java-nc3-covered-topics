244f425703cb9f3ef19cdbea9187b4416158cc65
Your Full Name <paneslawrence8@gmail.com>
2025-09-09 13:18:31 +0800

    Polymorphism + Abstraction

---
+++ day 27/Abstract_Car.java
@@ -0,0 +1,15 @@
+
+package com.joysistvi.abstraction.day27;
+
+
+public class Car extends LivingOrganism {
+
+    @Override
+    public void Accelerate() {
+    }
+
+    @Override
+    public void Break() {
+    }
+    
+}
