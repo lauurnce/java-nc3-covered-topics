d9648450b079658cdb24b1bc09a44315c0069dc2
Your Full Name <paneslawrence8@gmail.com>
2025-09-09 13:14:50 +0800

    Polymorphism + Abstraction

---
+++ day 27/MAle.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.polymorphism.day27;
+
+
+public class MAle extends LivingOrganism{
+    String name = "Rain"; 
+    
+    public void displayName(){
+        System.out.println(this.name);
+    }
+    
+}
