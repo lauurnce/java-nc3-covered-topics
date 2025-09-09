294d168628ddcf742c4e8915da47a90cab292320
Your Full Name <paneslawrence8@gmail.com>
2025-09-09 13:16:43 +0800

    Polymorphism + Abstraction

---
+++ day 27/Abstraction_Vehicle.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstraction.day27;
+
+//abstract class 
+
+public abstract class LivingOrganism {
+    
+    public abstract void Accelerate();
+    public abstract void Break(); 
+    
+    
+//    String word; 
+//    String name;
+    
+//    //Abstract Method
+//    public abstract void walk();
+//    public abstract void eat();
+//    public abstract void jump();
+      
+    
+  
+//    //Non-abstract Method
+//    public void eat(){
+//        System.out.println("Eating..");
+//    }
+//    
+    //non access modifier: final, static, abstract
+    //final class cannot be inherited and overriden 
+    
+}
