bb9cabfe54bcd06fe4ba6d0f0bfc8842bc883b7a
Your Full Name <paneslawrence8@gmail.com>
2025-09-09 13:17:49 +0800

    Polymorphism + Abstraction

---
+++ day 27/Abstraction_Truck.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstraction.day27;
+
+/**
+ *
+ * @author Admin
+ */
+public class Dog extends LivingOrganism{
+    // TO IMPLEMENT: ALT + ENTER + ENTER
+    
+//    @Override
+//    public void eat(){
+//        System.out.println("Dog is eating....Bark bark");
+//    }
+//    
+//   @Override
+//   public void walk (){
+//       System.out.println("Dog is walking");
+//   }
+//   
+//   @Override
+//   public void jump(){
+//       System.out.println("Dog is jumping...");
+//   }
+
+    @Override
+    public void Accelerate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void Break() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
