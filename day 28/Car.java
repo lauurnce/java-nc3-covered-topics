26b06553baf526fc1f9cc4d2dff40ddc018c529d
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 14:47:57 +0800

    Abstraction

---
+++ day 28/Car.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstraction.day28;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Car {
+    
+    public abstract void displayColor(); 
+    public abstract void brand(); 
+    
+
+//    @Override
+//    public void Accelerate() { //method override from parent abstract class
+//        System.out.println("Du du du du");
+//    }
+//
+//    @Override
+//    public void Break() {
+//        System.out.println("iewkfpoawo");
+//    }
+    
+}
