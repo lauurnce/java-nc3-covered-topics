a928fb1c77a7cbbb4e1199c5377735a6c4b613ce
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 14:50:05 +0800

    Abstraction

---
+++ day 28/2_Dog.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstraction.day28;
+
+/**
+ *
+ * @author Admin
+ */
+public class Dog implements Animal {
+    
+    @Override
+    public void eat(){
+        System.out.println("");}
+    @Override
+    public void walk(){
+        System.out.println("");} 
+    @Override
+    public void jump(){
+        System.out.println("");}
+    @Override
+    public void makeSound(String sound){
+        System.out.println("");} 
+    
+    @Override
+    public void crawl(){
+        System.out.println("");}
+}
