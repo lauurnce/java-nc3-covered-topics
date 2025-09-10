373dd98827b1f788d1893ffd6d130d678bc97c36
Your Full Name <paneslawrence8@gmail.com>
2025-09-10 14:45:48 +0800

    Abstraction

---
+++ day 28/Vehicle.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstraction.day28;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Vehicle {
+    
+      public void countWheels(); 
+      public void displayColor();
+//    public void eat(){
+//        
+//    }
+//    public abstract void Accelerate();
+//    public abstract void Break(); 
+}
