af7ee9823305b886bd8132666b7ff044a60d6849
Your Full Name <paneslawrence8@gmail.com>
2025-09-13 23:45:06 +0800

    Abstraction

---
+++ day 30/Transaction.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.joysistvi.abstractionactivity.day28;
+
+/**
+ *
+ * @author Admin
+ */
+public interface Transaction {
+    void deposit(double amount);
+    void withdraw(double amount);
+}
